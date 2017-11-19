2bcec9fa8a33520db3f10969477a620ada1d5ae0
DarkNormal <mark.lordan@me.com>
2017-11-19 19:07:48 +0000

    DatePickerDialog added for date selection

---
+++ app/src/main/java/com/marklordan/airgead/ui/addTransaction/DatePickerFragment.java
@@ -0,0 +1,33 @@
+package com.marklordan.airgead.ui.addTransaction;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
+
+/**
+ * Created by Mark on 19/11/2017.
+ */
+
+public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        return new DatePickerDialog(getActivity(), this, year,month, day);
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+    }
+}
