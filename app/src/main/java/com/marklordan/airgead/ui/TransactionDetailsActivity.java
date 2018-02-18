c6491957eedd3d6620d2e2ee793230757b1ac36c
DarkNormal <mark.lordan@me.com>
2018-02-18 16:40:58 +0000

    Expense / Income displayed correctly in edit transaction view

--- app/src/main/java/com/marklordan/airgead/ui/TransactionDetailsActivity.java
+++ app/src/main/java/com/marklordan/airgead/ui/TransactionDetailsActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -12,15 +13,19 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.marklordan.airgead.R;
 import com.marklordan.airgead.db.AirgeadContract;
+import com.marklordan.airgead.model.Expense;
+import com.marklordan.airgead.model.Income;
 import com.marklordan.airgead.model.Transaction;
 import com.marklordan.airgead.model.TransactionCategory;
 
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class TransactionDetailsActivity extends AppCompatActivity {
 
@@ -31,12 +36,14 @@ public class TransactionDetailsActivity extends AppCompatActivity {
     DecimalFormat mDecimalFormat = new DecimalFormat("#0.00");
     private SimpleDateFormat mDateFormat = new SimpleDateFormat("dd/MM/yyyy");
     private Transaction mTransaction;
+    private int mTransactionId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Bundle extras = getIntent().getExtras();
         mTransaction = extras.getParcelable("TRANSACTION");
+        mTransactionId = mTransaction.getId();
         setContentView(R.layout.activity_transaction);
 
         mDescriptionEditText = (EditText) findViewById(R.id.transaction_desc_input);
@@ -56,10 +63,21 @@ public class TransactionDetailsActivity extends AppCompatActivity {
         mTabLayout = (TabLayout) findViewById(R.id.tabs);
         mTabLayout.addTab(mTabLayout.newTab().setText("Expense"));
         mTabLayout.addTab(mTabLayout.newTab().setText("Income"));
-
+        TabLayout.Tab incomeTab = mTabLayout.getTabAt(1);
+        if(!mTransaction.isAnExpense()){
+            incomeTab.select();
+        }
         mTabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
+                switch (tab.getPosition()){
+                    case 0:
+                        mTransaction = new Expense();
+                        break;
+                    case 1:
+                        mTransaction = new Income();
+                        break;
+                }
             }
 
             @Override
@@ -95,10 +113,25 @@ public class TransactionDetailsActivity extends AppCompatActivity {
         switch (item.getItemId()){
             case R.id.confirm_item:
                 //TODO update item
-                getContentResolver().update(AirgeadContract.TransactionTable.CONTENT_URI,
-                        mTransaction.transactionToContentValues(),
-                        AirgeadContract.TransactionTable.Cols._ID + "= ?",
-                        new String[]{String.valueOf(mTransaction.getId())});
+                String enteredAmount = mAmountEditText.getText().toString();
+                String enteredTitle = mDescriptionEditText.getText().toString();
+                if(!enteredAmount.isEmpty() && !enteredTitle.isEmpty()) {
+                    mTransaction.setAmount(Double.valueOf(enteredAmount));
+                    mTransaction.setDescription(enteredTitle);
+                    mTransaction.setCategory(TransactionCategory.fromInteger(mCategorySpinner.getSelectedItemPosition()));
+                    if (mTransaction.getDateOfTransaction() == null) {
+                        mTransaction.setDateOfTransaction(new Date());
+                    }
+                    mTransaction.setId(mTransactionId);
+                    getContentResolver().update(AirgeadContract.TransactionTable.CONTENT_URI,
+                            mTransaction.transactionToContentValues(),
+                            AirgeadContract.TransactionTable.Cols._ID + "= ?",
+                            new String[]{String.valueOf(mTransaction.getId())});
+                    finish();
+                }
+                else{
+                    Toast.makeText(TransactionDetailsActivity.this, "A transaction needs at least a name and a value!", Toast.LENGTH_SHORT).show();
+                }
             default:
                 return super.onOptionsItemSelected(item);
         }
