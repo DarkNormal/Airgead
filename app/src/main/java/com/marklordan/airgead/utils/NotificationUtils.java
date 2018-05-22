8491209f117853a18749ec5f3c9c1b589a1c1264
DarkNormal <mark.lordan@me.com>
2018-05-22 20:17:48 +0100

    Notification Preferences

--- app/src/main/java/com/marklordan/airgead/utils/NotificationUtils.java
+++ app/src/main/java/com/marklordan/airgead/utils/NotificationUtils.java
@@ -4,6 +4,8 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 
 import com.marklordan.airgead.service.NotificationReceiver;
 
@@ -13,7 +15,11 @@ import java.util.Calendar;
  * Created by Mark on 13/05/2018.
  */
 
+
 public class NotificationUtils {
+
+    private static final String TAG = NotificationUtils.class.getSimpleName();
+    private static final int AIRGEAD_NOTIFICATION_REQUEST_CODE = 2911;
 
     public static void scheduleNotification(Context context){
         Calendar cal = Calendar.getInstance();
@@ -21,9 +27,18 @@ public class NotificationUtils {
         cal.set(Calendar.HOUR_OF_DAY, 20);
         PendingIntent pendingIntent;
         Intent intent = new Intent(context, NotificationReceiver.class);
-        pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
+        pendingIntent = PendingIntent.getBroadcast(context, AIRGEAD_NOTIFICATION_REQUEST_CODE, intent, 0);
         AlarmManager manager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         //TODO - alarm repeats very regularly as a test for development, for release it must use cal
         manager.setInexactRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), 8000, pendingIntent);
+        Log.d(TAG, "Recurring Notifications scheduled");
+    }
+
+    public static void cancelNotification(Context context){
+        Intent intent = new Intent(context, NotificationReceiver.class);
+        PendingIntent sender = PendingIntent.getBroadcast(context, AIRGEAD_NOTIFICATION_REQUEST_CODE, intent, 0);
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        alarmManager.cancel(sender);
+        Log.d(TAG, "Recurring Notifications cancelled by user");
     }
 }
